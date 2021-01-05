8a469bbf906ace0dfd8645ec92db4aa58e3d8b6f
KuptNoodlzFTW <77007948+KuptNoodlzFTW@users.noreply.github.com>
2021-01-05 13:45:51 -0500

    Update slimeDude.java

--- slimeDude.java
+++ slimeDude.java
@@ -4,6 +4,7 @@ public class slimeDude {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( "jerry sucks" );
+		System.out.println("no u");
 	}
 
 }
