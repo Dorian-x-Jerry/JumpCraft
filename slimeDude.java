db1325ddb92a38e0b0611f416a958697cbabda2b
dourian <75771131+dourian@users.noreply.github.com>
2021-01-05 23:18:30 -0500

    Update slimeDude.java

--- slimeDude.java
+++ slimeDude.java
@@ -142,7 +142,7 @@ public class slimeDude extends JPanel implements Runnable, KeyListener {
 	void checkCollision(Rectangle wall) {
 		//check if player touches wall
 		if (player.intersects(wall)) {
-			System.out.println("collision");
+			//System.out.println("collision");
 			//stop the player from moving
 			double left1 = player.getX();
 			double right1 = player.getX() + player.getWidth();
@@ -153,22 +153,33 @@ public class slimeDude extends JPanel implements Runnable, KeyListener {
 			double top2 = wall.getY();
 			double bottom2 = wall.getY() + wall.getHeight();
 			
+			
 			if (right1 > left2 && left1 < left2 && right1 - left2 < bottom1 - top2 && right1 - left2 < bottom2 - top1) {
 				player.x = wall.x - player.width;
 				airborne = true;
+				//yVel-=0.1;
+				System.out.println("left");
 			}
 	        else if (left1 < right2 && right1 > right2 && right2 - left1 < bottom1 - top2 && right2 - left1 < bottom2 - top1) {
 	        	player.x = wall.x + wall.width;
 	        	airborne = true;
+	        	//yVel-=0.1;
+	        	System.out.println("right");
 	        }
 	        else if (bottom1 > top2 && top1 < top2) {
 	        	player.y = wall.y - player.height;
 				airborne = false;
+				System.out.println("top");
 	        }
 	        else if (top1 < bottom2 && bottom1 > bottom2) {
 	        	player.y = wall.y + wall.height;
 	        	airborne = true;
+	        	yVel/=3;
+	        	System.out.println("bottom");
 	        }
+		}
+		else {
+			airborne=true;
 		}
 	}
 	public static void main(String[] args) {
